d94d8cc6e5f076fce534762049053449ff0e3c99
Thomas GENDRON <thomas.gendron2000@gmail.com>
2024-04-27 01:58:29 +0200

    [FRONT] Disable Strict Mode

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
@@ -8,20 +8,20 @@ import App from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<ContextProvider>
-			<BrowserRouter>
-				<div className='flex justify-center w-full'>
-					<div className='flex flex-col items-center w-full max-w-6xl gap-8 min-h-screen'>
-						<NavBar />
-						<Routes>
-							<Route path='/' element={<App />} />
-							<Route path='*' element={<App />} />
-							<Route path='/login' element={<Login />} />
-						</Routes>
-					</div>
+	// <React.StrictMode>
+	<ContextProvider>
+		<BrowserRouter>
+			<div className='flex justify-center w-full'>
+				<div className='flex flex-col items-center w-full max-w-6xl gap-8 min-h-screen'>
+					<NavBar />
+					<Routes>
+						<Route path='/' element={<App />} />
+						<Route path='*' element={<App />} />
+						<Route path='/login' element={<Login />} />
+					</Routes>
 				</div>
-			</BrowserRouter>
-		</ContextProvider>
-	</React.StrictMode>,
+			</div>
+		</BrowserRouter>
+	</ContextProvider>,
+	// </React.StrictMode>,
 );
